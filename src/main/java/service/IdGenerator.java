5df26e628ffbbe9d7dff3245ab9e19cac0a469ce
Matthew <matthewvaulin@gmail.com>
2026-05-01 03:30:19 +0500

    Edit IdGenerator.java

--- src/main/java/service/IdGenerator.java
+++ src/main/java/service/IdGenerator.java
@@ -1,10 +1,16 @@
 package service;
 
 public class IdGenerator {
-   private long currentId = 1;
+    private long currentId = 1;
 
-   public long nextId() {
-       return currentId++;
-   }
+    public long nextId() {
+        return currentId++;
+    }
 
+    public void setNextId(long nextId) {
+        if (nextId < 1) {
+            throw new IllegalArgumentException("nextId должен быть положительным");
+        }
+        this.currentId = nextId;
+    }
 }
