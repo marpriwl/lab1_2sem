78f81d01ec5b99c9055ddb08b4229100a453e5b4
Matthew <matthewvaulin@gmail.com>
2026-05-01 04:21:18 +0500

    Fixed the mistake (Main.java)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 import cli.LabCli;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public static void main(String[] args) {
-    new LabCli(args).start();
+public class Main {
+    public static void main(String[] args) {
+        new LabCli(args).start();
+    }
 }
